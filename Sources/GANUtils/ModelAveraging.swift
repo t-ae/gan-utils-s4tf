18efb22731de58f95627e68831e3e06374b6ed60
araki <araki@qoncept.co.jp>
2020-03-18 16:40:41 +0900

    Parameter averaging

--- Sources/GANUtils/ModelAveraging.swift
+++ Sources/GANUtils/ModelAveraging.swift
@@ -10,6 +10,14 @@ public class ModelAveraging<Model: Layer> {
     public init(average: Model, beta: Float = 0.95) {
         self.average = average
         self.beta = beta
+        
+        // Separate `Parameter`s from base model
+        for kp in self.average.recursivelyAllWritableKeyPaths(to: Parameter<Float>.self) {
+            self.average[keyPath: kp] = Parameter(average[keyPath: kp].value)
+        }
+        for kp in average.recursivelyAllWritableKeyPaths(to: Parameter<Double>.self) {
+            self.average[keyPath: kp] = Parameter(average[keyPath: kp].value)
+        }
     }
     
     public func update(model: Model) {
@@ -18,6 +26,16 @@ public class ModelAveraging<Model: Layer> {
         }
         for kp in model.recursivelyAllWritableKeyPaths(to: Tensor<Double>.self) {
             average[keyPath: kp] = lerp(model[keyPath: kp], average[keyPath: kp], rate: Double(beta))
+        }
+        for kp in model.recursivelyAllWritableKeyPaths(to: Parameter<Float>.self) {
+            average[keyPath: kp].value = lerp(model[keyPath: kp].value,
+                                              average[keyPath: kp].value,
+                                              rate: beta)
+        }
+        for kp in model.recursivelyAllWritableKeyPaths(to: Parameter<Double>.self) {
+            average[keyPath: kp].value = lerp(model[keyPath: kp].value,
+                                              average[keyPath: kp].value,
+                                              rate: Double(beta))
         }
     }
 }
--- Tests/GANUtilsTests/ModelAveragingTests.swift
+++ Tests/GANUtilsTests/ModelAveragingTests.swift
@@ -10,10 +10,11 @@ class ModelAveragingTests: XCTestCase {
                                       activation: identity)
             var dense2 = Dense<Float>(weight: Tensor<Float>(zeros: [10, 1]),
                                       activation: identity)
+            var param = Parameter(Tensor<Float>(zeros: [3, 3]))
             
             @differentiable
             func callAsFunction(_ input: Tensor<Float>) -> Tensor<Float> {
-                dense2(dense1(input))
+                dense2(dense1(input)) * param.value
             }
         }
         
@@ -22,12 +23,17 @@ class ModelAveragingTests: XCTestCase {
         let avg = ModelAveraging(average: model, beta: 0.95)
         XCTAssertEqual(avg.average.dense1.weight, model.dense1.weight)
         XCTAssertEqual(avg.average.dense2.weight, model.dense2.weight)
+        XCTAssertEqual(avg.average.param.value, model.param.value)
         
         model.dense1.weight = Tensor<Float>(ones: [10, 10])
         model.dense2.weight = Tensor<Float>(ones: [10, 1])
+        model.param.value = Tensor<Float>(ones: [3, 3])
         avg.update(model: model)
         
         XCTAssert(avg.average.dense1.weight.isAlmostEqual(to: Tensor(repeating: 0.05, shape: [10, 10])))
         XCTAssert(avg.average.dense2.weight.isAlmostEqual(to: Tensor(repeating: 0.05, shape: [10, 1])))
+        print(model.param)
+        print(avg.average.param.value)
+        XCTAssert(avg.average.param.value.isAlmostEqual(to: Tensor(repeating: 0.05, shape: [3, 3])))
     }
 }
