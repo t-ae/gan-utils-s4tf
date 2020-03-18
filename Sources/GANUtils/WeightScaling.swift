ce8f4466c9c603c799a39290e7a24c5b1f648075
araki <araki@qoncept.co.jp>
2020-03-18 15:41:29 +0900

    Add SN/WSTransposedConv2D

--- Sources/GANUtils/WeightScaling.swift
+++ Sources/GANUtils/WeightScaling.swift
@@ -140,74 +140,75 @@ public extension WSConv2D {
     }
 }
 
-//public struct WSTransposedConv2D<Scalar: TensorFlowFloatingPoint>: Layer {
-//    public var filter: Tensor<Scalar>
-//    @noDerivative
-//    public let scale: Scalar
-//    public var bias: Tensor<Scalar>
-//    @noDerivative public let activation: Activation
-//    @noDerivative public let strides: (Int, Int)
-//    @noDerivative public let padding: Padding
-//    @noDerivative public let paddingIndex: Int
-//    @noDerivative private let useBias: Bool
-//    
-//    public typealias Activation = @differentiable (Tensor<Scalar>) -> Tensor<Scalar>
-//    
-//    public init(
-//        filter: Tensor<Scalar>,
-//        bias: Tensor<Scalar>? = nil,
-//        activation: @escaping Activation = identity,
-//        strides: (Int, Int) = (1, 1),
-//        padding: Padding = .valid,
-//        enableWeightScaling: Bool = true
-//    ) {
-//        scale = enableWeightScaling ? filter.standardDeviation().scalarized() : 1
-//        self.filter = filter / scale
-//        self.bias = bias ?? .zero
-//        self.activation = activation
-//        self.strides = strides
-//        self.padding = padding
-//        self.paddingIndex = padding == .same ? 0 : 1
-//        useBias = (bias != nil)
-//    }
-//    
-//    @differentiable
-//    public func callAsFunction(_ input: Tensor<Scalar>) -> Tensor<Scalar> {
-//        let batchSize = input.shape[0]
-//        let h = (input.shape[1] - (1 * paddingIndex)) * strides.0 + (filter.shape[0] * paddingIndex)
-//        let w = (input.shape[2] - (1 * paddingIndex)) * strides.1 + (filter.shape[1] * paddingIndex)
-//        let c = filter.shape[2]
+public struct WSTransposedConv2D<Scalar: TensorFlowFloatingPoint>: Layer {
+    public var filter: Tensor<Scalar>
+    @noDerivative
+    public let scale: Scalar
+    public var bias: Tensor<Scalar>
+    @noDerivative public let activation: Activation
+    @noDerivative public let strides: (Int, Int)
+    @noDerivative public let padding: Padding
+    @noDerivative public let paddingIndex: Int
+    @noDerivative private let useBias: Bool
+    
+    public typealias Activation = @differentiable (Tensor<Scalar>) -> Tensor<Scalar>
+    
+    public init(
+        filter: Tensor<Scalar>,
+        bias: Tensor<Scalar>? = nil,
+        activation: @escaping Activation = identity,
+        strides: (Int, Int) = (1, 1),
+        padding: Padding = .valid,
+        enableWeightScaling: Bool = true
+    ) {
+        scale = enableWeightScaling ? filter.standardDeviation().scalarized() : 1
+        self.filter = filter / scale
+        self.bias = bias ?? .zero
+        self.activation = activation
+        self.strides = strides
+        self.padding = padding
+        self.paddingIndex = padding == .same ? 0 : 1
+        useBias = (bias != nil)
+    }
+    
+    @differentiable
+    public func callAsFunction(_ input: Tensor<Scalar>) -> Tensor<Scalar> {
+        let batchSize = input.shape[0]
+        let h = (input.shape[1] - (1 * paddingIndex)) * strides.0 + (filter.shape[0] * paddingIndex)
+        let w = (input.shape[2] - (1 * paddingIndex)) * strides.1 + (filter.shape[1] * paddingIndex)
+        let c = filter.shape[2]
 //        let newShape = [Int64(batchSize), Int64(h), Int64(w), Int64(c)]
-//        let conv = transposedConv2D(
-//            input,
-//            shape: newShape,
-//            filter: scale * filter,
-//            strides: (1, strides.0, strides.1, 1),
-//            padding: padding)
-//        return activation(useBias ? (conv + bias) : conv)
-//    }
-//}
-//
-//extension WSTransposedConv2D {
-//    public init(
-//        filterShape: (Int, Int, Int, Int),
-//        strides: (Int, Int) = (1, 1),
-//        padding: Padding = .valid,
-//        activation: @escaping Activation = identity,
-//        useBias: Bool = true,
-//        enableWeightScaling: Bool = true,
-//        filterInitializer: ParameterInitializer<Scalar> = glorotUniform(),
-//        biasInitializer: ParameterInitializer<Scalar> = zeros()
-//    ) {
-//        let filterTensorShape = TensorShape([
-//            filterShape.0, filterShape.1, filterShape.2, filterShape.3,
-//        ])
-//        self.init(
-//            filter: filterInitializer(filterTensorShape),
-//            bias: useBias ? biasInitializer([filterShape.2]) : nil,
-//            activation: activation,
-//            strides: strides,
-//            padding: padding,
-//            enableWeightScaling: enableWeightScaling)
-//    }
-//}
+        let newShape = Tensor<Int32>([Int32(batchSize), Int32(h), Int32(w), Int32(c)])
+        let conv = transposedConv2D(
+            input,
+            shape: newShape,
+            filter: scale * filter,
+            strides: (1, strides.0, strides.1, 1),
+            padding: padding)
+        return activation(useBias ? (conv + bias) : conv)
+    }
+}
+
+extension WSTransposedConv2D {
+    public init(
+        filterShape: (Int, Int, Int, Int),
+        strides: (Int, Int) = (1, 1),
+        padding: Padding = .valid,
+        activation: @escaping Activation = identity,
+        useBias: Bool = true,
+        enableWeightScaling: Bool = true,
+        filterInitializer: ParameterInitializer<Scalar> = glorotUniform(),
+        biasInitializer: ParameterInitializer<Scalar> = zeros()
+    ) {
+        let filterTensorShape = TensorShape([
+            filterShape.0, filterShape.1, filterShape.2, filterShape.3,
+        ])
+        self.init(
+            filter: filterInitializer(filterTensorShape),
+            bias: useBias ? biasInitializer([filterShape.2]) : nil,
+            activation: activation,
+            strides: strides,
+            padding: padding,
+            enableWeightScaling: enableWeightScaling)
+    }
+}
